52ef641e5c22f52febfd80adcd376d7b0a73a8da
hitzhangjie <hit.zhangjie@gmail.com>
2021-03-30 01:53:02 +0800

    generate the book

---
+++ publish-gitbook2ghpages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+rm -rf docs
+gitbook build . docs
+
+mv docs/.gitbook/assets docs/assets
+rm -rf docs/.gitbook
+
+find docs/ -iname "*.html" | xargs sed -i 's/.gitbook\/assets/assets/g'
+
+git add docs
+git cc -m 'publish book'
+
+git push
