b9de0d82d8099176454b82d81884d29d784300cd
hitzhangjie <hit.zhangjie@gmail.com>
2022-03-27 08:48:45 +0800

    publish book

---
+++ docs/publish-gitbook2ghpages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+rm -rf docs
+gitbook build . docs
+
+mv docs/.gitbook/assets docs/assets
+rm -rf docs/.gitbook
+
+find docs/ -iname "*.html" | xargs sed -i 's/.gitbook\/assets/assets/g'
+find docs/ -iname "*.html" | xargs sed -i 's/\\_/_/g'
+find docs/ -iname "*.html" | xargs sed -i 's/\/_/_/g'
+
+git add docs
+git cc -m 'publish book'
+
+git push
