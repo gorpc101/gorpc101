9cfd3ecb966775d0a940cc5d47ca3b9856a57141
hitzhangjie <hit.zhangjie@gmail.com>
2023-02-16 14:13:38 +0800

    publish book

--- docs/publish-gitbook2ghpages.sh
+++ docs/publish-gitbook2ghpages.sh
@@ -1,7 +1,35 @@
 #!/bin/bash -e
 
 rm -rf docs
-gitbook build . docs
+
+USE_DOCKER=0
+# Parse command line options
+while getopts "dh" opt; do
+    case "$opt" in
+    d)  # use docker container to build
+        USE_DOCKER=1
+        echo "docker flag set"
+        ;;
+    h)  # Display help
+        echo "Usage: ./publish-gitbook2ghpages.sh [-d] [-h]"
+        echo "-d: use docker container hitzhangjie/gitbook-cli to build pages"
+        echo "    otherwise, use native gitbook to build pages"
+        echo "-h: show the help message"
+        exit 0
+        ;;
+    *)  # Invalid option
+        echo "Error: Invalid option $opt"
+        exit 1
+        ;;
+    esac
+done
+
+if [ $USE_DOCKER -eq 1 ]
+then
+    docker run --rm -v $(pwd -P):/root/gitbook hitzhangjie/gitbook-cli:latest gitbook build . docs
+else
+    gitbook build . docs
+fi
 
 mv docs/.gitbook/assets docs/assets
 rm -rf docs/.gitbook
